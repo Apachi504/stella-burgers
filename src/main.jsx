b287f88bba2609691aed58ae0fd098bc3f31bff2
Apachi504 <apachi504@gmail.com>
2024-11-19 11:53:56 +0300

    Готовы проект по первому спринту

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from '../src/components/app/app.jsx'
+import './reset.module.scss'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
