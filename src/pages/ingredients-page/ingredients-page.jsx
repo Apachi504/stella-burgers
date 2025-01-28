8acaf18177beb26da82ea3e9c5f091264359d97a
Apachi504 <apachi504@gmail.com>
2025-01-28 21:36:11 +0300

    create Modal and ModalPages

---
+++ src/pages/ingredients-page/ingredients-page.jsx
@@ -0,0 +1,22 @@
+import {useParams} from "react-router-dom";
+import React, { useMemo} from "react";
+import IngredientDetails from "../../components/burger-ingredients/ingredient-details/ingredient-details.jsx";
+import {useSelector} from "react-redux";
+import {getAllIngredients} from "../../services/burger-ingredients/burger-ingredients-slice.js";
+import styles from "./ingredients-info.module.scss";
+export const IngredientPage = () => {
+    const {id} = useParams();
+    const burgerIngredients = useSelector(getAllIngredients);
+
+    const ingredient = useMemo(() => {
+        return burgerIngredients?.data?.find((item) => item._id === id);
+    }, [burgerIngredients.data, id]);
+
+    return (
+        ingredient && <section className={styles.container}>
+            <h2 className='text text_type_main-large mb-4'>Детали ингредиента</h2>
+            <IngredientDetails ingredient={ingredient}/>
+        </section>
+
+    );
+};
